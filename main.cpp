3a004c30853d69b3206625bf7809c59720b64675
giannaajello <64743235+giannaajello@users.noreply.github.com>
2020-10-09 03:32:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,18 @@
 
 
 #include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decryption.h"
 
 int main()
 {
+  
+  test_ascii("Hello, world!");
+  std::cout << encryptCaesar("Hello, world!", 10) << std::endl;
+  std::cout << encryptVigenere("Hello, world!", "sushi") << std::endl;
+  std::cout << decryptCaesar("IFJIFQZX!", 5) << std::endl;
+  std::cout << decryptVigenere("Zydsw, oijsl!", "sushi") << std::endl;
+  
   return 0;
 }
