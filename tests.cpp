1d7319d4571a9c498778cafbbb2bcc6cca334651
giannaajello <64743235+giannaajello@users.noreply.github.com>
2020-10-09 03:48:10 -0400

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,46 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_eIMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "caesar.h"
 #include "funcs.h"
+#include "vigenere.h"
+#include "decryption.h"
 
-// add your tests here
+// add your tests her
+TEST_CASE("shift char")
+{
+  CHECK(shiftChar('a',3) == 'd');
+  CHECK(shiftChar('C',0) == 'C');
+  CHECK(shiftChar('B',-1) == 'A');
+  CHECK(shiftChar('d', 3) == 'g');
+}
+TEST_CASE("encrypt caesar")
+{
+  CHECK(encryptCaesar("Hello, world!",10)=="Rovvy, Gybvn!");
+  CHECK(encryptCaesar("Computer science!",7)=="Jvtwbaly zjplujl!");
+  CHECK(encryptCaesar("DAEDALUS.",5)=="IFJIFQZX.");
+  CHECK(encryptCaesar("!",3)=="!");
+}
+
+TEST_CASE("encrypt vigenere")
+{
+  CHECK(encryptVigenere("How are you?","food") == "Mck dws mrz?");
+  CHECK(encryptVigenere("GUMBALL!","amazing") == "GGMAIYR!");
+  CHECK(encryptVigenere("#@^%@&^&!*","hello") == "#@^%@&^&!*");
+  CHECK(encryptVigenere("taco tuesday..","best") == "ueuh uywleeq..");
+ }			
+
+TEST_CASE("decrypt caesar")
+{
+  CHECK(decryptCaesar("xuttpd xjfxts",5) == "spooky season");
+  CHECK(decryptCaesar("EPIB QA BPQA?!",8) == "WHAT IS THIS?!");
+  CHECK(decryptCaesar("@#",6) == "@#");
+  CHECK(decryptCaesar("qjuuxfnnw!!!!!",9) == "halloween!!!!!");
+}
+
+TEST_CASE("decrypt vigenere")
+{
+   CHECK(decryptVigenere("wwaqog q. rense","king") == "monkey d. luffy");
+   CHECK(decryptVigenere("%&*^&*","rando") == "%&*^&*");
+   CHECK(decryptVigenere("XJWAMCA & HSEJ","iconic") == "PHINEAS & FERB");
+   CHECK(decryptVigenere("DITCR slik lo yx!!","weird") == "HELLO what is up!!");
+}
