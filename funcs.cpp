4f58a351207426389778ced323fa884f87aa9ac8
giannaajello <64743235+giannaajello@users.noreply.github.com>
2020-10-09 03:34:33 -0400

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include "funcs.h"
 
-// add functions here
+void test_ascii(std::string s)
+{
+
+  for (int i = 0; i < s.length(); i++) //starts at beginning of string and goes to the end
+    {
+      std::cout<<s[i]<<(int)s[i]<<std::endl; //goes through each character in the string,
+      //casts char as int for printing
+    }
+}
 
